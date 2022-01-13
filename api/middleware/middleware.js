306612593b71e899c9f882d15f942efa633c764f
arsalan rahimi <years1373@yahoo.com>
2022-01-12 21:52:29 -0800

    almost half done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,37 @@
+const User = require("../users/users-model.js")
 function logger(req, res, next) {
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
   // DO YOUR MAGIC
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({ message: "User not found" })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({ message: "problem finding user" })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  next()
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = { logger, validateUserId, validateUser, validatePost }
